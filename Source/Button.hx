5c4e4c6916d8a0c800034cd6e2c37fda1e6fd23a
Alexey Power <vulturetm@gmail.com>
2020-01-10 09:34:56 +0300

    sync work

---
+++ Source/Button.hx
@@ -0,0 +1,24 @@
+package;
+
+class Button
+{
+	public function new()
+	{
+
+	}
+
+	public function preInit():Array<Dynamic>
+	{
+		
+	}
+
+	public function init():Array<Dynamic>
+	{
+
+	}
+
+	public function postInit():<Dynamic>
+	{
+
+	}
+}
