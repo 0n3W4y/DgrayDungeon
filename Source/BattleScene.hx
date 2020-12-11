2890a8ed443b93bcf0d8df01cbae1394b4b17d7b
Alexey Power <vulturetm@gmail.com>
2020-12-11 16:16:00 +0300

    Update BattleScene.hx
    sycn work

--- Source/BattleScene.hx
+++ Source/BattleScene.hx
@@ -70,14 +70,20 @@ class BattleScene extends Scene
         if( this._enemyEvents <= 0 || this._enemyEvents == null )
             throw '$error Enemy events is not valid. $_enemyEvents';
 
-        //treasure;
-        //traps;
+        if( this._treasureEvents <= 0 || this._treasureEvents == null )
+            throw '$error Treasure events is not valid. $_treasureEvents';
+
+        if( this._trapEvents <= 0 || this._trapEvents == null )
+            throw '$error Traps events is not valid. $_trapEvents';
 
         if( this._quest == null )
             throw '$error Quest is NULL!!';
 
         if( this._numOfBackgroundImages <= 1 || this._numOfBackgroundImages == null )
             throw '$error Background images are not valid number. $_numOfBackgroundImages';
+
+        if(( this._enemyEvents + this._treasureEvents + this._trapEvents ) >= Math.floor( this._dungeonLength / 3 ))
+            throw '$error Map events more than dungeon length!!!! ';
         //TODO: check all fields;
     }
 
@@ -115,28 +121,96 @@ class BattleScene extends Scene
 
     public function generate():Void
     {
-        //TODO build an array, with 0, 1 , 2 etries; 0 - nothing, 1 - battle, 2 - treasure;
-        var dungeonLength:Int = this._dungeonLength - 1;
-        var enemyEvents:Int = this._enemyEvents;
-        var treasureEvents:Int = this._treasureEvents;
-        var trapEvents:Int = this._trapEvents;
+        var dungeonLength:Int = this._dungeonLength - 1; // starting array with 0;
+        this._generatedMap = [ for( i in 0...dungeonLength ) 0 ];
+        
 
-        for( i in 0...dungeonLength )
+        this._generateEventsOnGeneratedMap( "enemy" );
+        this._generateEventsOnGeneratedMap( "treasure" );
+        this._generateEventsOnGeneratedMap( "trap" );
+
+    }
+
+    public function move( direction:String ):Void
+    {
+        //TODO: move left or right, so triggered function will be chage curretn position, and change graphicsSprite;
+        // get array of graphics ( length ? ) find random index, get sprite child with that index, get width of current, put x with that width; y = 0; always;
+        // 
+    }
+
+
+
+    public function getEventFromNextPosition( direction:String ):Int
+    {
+        var currentPosition:Int = this._playerCurrentPosition;
+        switch( direction )
         {
-            this._generatedMap[ i ] = 0; // fill array with zeros;
+            case "left": currentPosition -1;
+            case "right": currentPosition +1;
+            default: 'Error in BattleScene.getEventFromNextPosition Can not get event from "$direction" position!';
         }
 
-        //try to randomly spawn events;
-        while( enemyEvents > 0 )
+        if( currentPosition < 0 || currentPosition >= this._generatedMap.length )
+            return -1;
+
+        return this._generatedMap[ currentPosition ];
+    }
+
+    public function canMoveToNextPosition( direction:String ):Bool
+    {
+        var currentPosition:Int = this._playerCurrentPosition;
+        switch( direction )
         {
-            var randomIndex:Int = Math.floor( 1 + Math.random() * ( dungeonLength - 1 ));
-            if(  this._generatedMap[ randomIndex ] == 0 )
+            case "left": currentPosition -1;
+            case "right": currentPosition +1;
+            default: 'Error in BattleScene.canMoveToNextPosition Can not check "$direction" position!';
+        }
+
+        if( currentPosition < 0 || currentPosition >= this._generatedMap.length )
+            return false;
+        else
+            return true;
+    }
+
+    public function moveToTheNextPosition( direction:String ):Void
+    {
+        if( this.canMoveToNextPosition( direction ) )
+        {
+            switch( direction )
             {
-                this._generatedMap[ randomIndex ] = 1; // enemy event == 1;
-                enemyEvents--;
+                case "left": this._playerCurrentPosition--;
+                case "right": this._playerCurrentPosition++;
+                default: 'Error in BattleScene. Can not move to "$direction" position!';
             }
         }
+    }
 
+
+
+
+    //PRIVATE
+
+    private function _generateEventsOnGeneratedMap( event:String ):Void
+    {
+        var events:Int = 0;
+        var eventNumber:Int = 0;
+        switch( event )
+        {
+            case "enemy": events = this._enemyEvents; eventNumber = 1;
+            case "treasure": events = this._treasureEvents; eventNumber = 2;
+            case "trap": events = this._trapEvents; eventNumber = 3;
+            default: throw 'Error in battleScene._generateEventsOnGeneratedMap. Can not generate event with name:"$event"';
+        }
+
+        while( events > 0 )
+        {
+            var randomIndex:Int = Math.floor( 1 + Math.random() * ( this._generatedMap.length - 2 )); // 2 baceuse -1 and -1; last sector will be exit;
+            if(  this._generatedMap[ randomIndex ] == 0 )
+            {
+                this._generatedMap[ randomIndex ] = eventNumber; // enemy event == 1;
+                events--;
+            }
+        }
     }
 
 }
